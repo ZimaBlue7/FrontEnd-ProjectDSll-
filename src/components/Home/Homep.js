f27e4b2aa5ffa8ab2b9b8857b0fca17cfc0f033c
Juan David <juan.getial@correounivalle.edu.co>
2022-11-10 15:54:21 -0500

    V1.1 Nabvar muy sencilla

---
+++ src/components/Home/Homep.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const Homep = () => {
+  return (
+    <>
+    <section>
+        {/* seccion nabvar */}
+        <Navbar/>
+    </section>
+    </>
+  )
+}
+
+export default Homep
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Homep from './components/Home/Homep';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Homep/>
   </React.StrictMode>
 );
